6dc82931cd6c72e4b6da10b907a20d02fa1897ba
seljaziri <souhaib.aljaziri@gmail.com>
2024-06-07 01:31:59 +0100

    update step2

--- react/src/App.js
+++ react/src/App.js
@@ -1,29 +1,11 @@
-import React, { useState } from 'react';
-import Login from './components/Login';
-import HomePage from './components/Home';
+import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import Quizz from './components/Quizz';
 const App = () => {
-  // State to manage login status
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-  // Function to handle login
-  const handleLogin = () => {
-    // Implement your login logic here
-    // For example, set isLoggedIn to true after successful login
-    setIsLoggedIn(true);
-  };
-
-  // Function to handle logout
-  const handleLogout = () => {
-    // Implement your logout logic here
-    // For example, set isLoggedIn to false after logout
-    setIsLoggedIn(false);
-  };
 
   return (
     <div>
-      {isLoggedIn ? <HomePage onLogout={handleLogout} /> : <Login onLogin={handleLogin} />}
+      <Quizz />
     </div>
   );
 };
